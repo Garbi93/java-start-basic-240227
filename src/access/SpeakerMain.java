ad8d5ed95d81bb955b036e4efb9083d89b0db225
Garbi93 <iseung2093@gmail.com>
2024-03-07 15:30:19 +0900

    3/07 private 처리 이유

--- src/access/SpeakerMain.java
+++ src/access/SpeakerMain.java
@@ -13,7 +13,7 @@ public class SpeakerMain {
 
         // 필드에 직접 접근
         System.out.println("volume 필드 직접 접근 수정");
-        speaker.volume = 200;
+        // speaker.volume = 200; // private 처리한 맴버변수에 접근하려 해서 사용 불가능 하다.
         speaker.showVolume();
     }
 }
