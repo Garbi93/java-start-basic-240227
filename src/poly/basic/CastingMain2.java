5d82c357b31d14b4fdbd7225b7a8860ee2120e03
Garbi93 <iseung2093@gmail.com>
2024-03-19 13:12:25 +0900

    03/19 캐스팅의 종류

---
+++ src/poly/basic/CastingMain2.java
@@ -0,0 +1,17 @@
+package poly.basic;
+
+public class CastingMain2 {
+    public static void main(String[] args) {
+        // 부모 변수가 자식 인스턴스 참조 (다형적 참조)
+        Parent poly = new Child();
+        // 단 자식의 기능은 호출할 수 없다.
+        // poly.childMethod(); // <- 컴파일 오류가 발생
+
+        // 다움캐스팅 (부모타입 -> 자식타입)
+        Child child = (Child) poly; // <- 이렇게 사용하면 강제로 부모티입을 자식타입으로 변환 시킨다.
+        child.childMethod();
+
+        // 일시적 다운캐스팅 - 해당 메서드를 호출하는 순간만 다운캐스팅
+        ((Child) poly).childMethod();
+    }
+}
